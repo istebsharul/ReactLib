84c319dcc3ae40489e43c65189534148c4e2840f
istebsharul <istebsharulbari919@gmail.com>
2023-10-16 20:07:04 +0530

    del

---
+++ react-router/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
